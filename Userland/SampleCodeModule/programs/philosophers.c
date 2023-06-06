61f99209aa18db2c250d354dd602b0063854efd7
davidwis <davidwis000@gmail.com>
2023-06-06 03:20:43 -0300

    Cambio de semaforos en removePhilosopher

--- Userland/SampleCodeModule/programs/philosophers.c
+++ Userland/SampleCodeModule/programs/philosophers.c
@@ -134,13 +134,11 @@ static int8_t addPhilosopher(int index) {
 static int8_t removePhilosopher(int index) {
 	printf("Se invita a retirarse a %s\n", philosopherNames[index]);
 	semWait(MUTEX_SEM_ID);
-	if (philosopherStates[left(index)] == EATING && philosopherStates[right(index)] == EATING) { // Estado invalido para remover
-		semPost(MUTEX_SEM_ID);																	 // Bloquearse hasta que el filosofo este comiendo
-		semWait(philosopherSemaphore(index));
-	}
-	else
+	while (philosopherStates[left(index)] == EATING && philosopherStates[right(index)] == EATING) { // Estado invalido para remover
 		semPost(MUTEX_SEM_ID);
-	semWait(MUTEX_SEM_ID);
+		semWait(philosopherSemaphore(index)); // Bloquearse hasta que el filosofo este comiendo
+		semWait(MUTEX_SEM_ID);
+	}
 	killProcess(philosopherPids[index]);
 	waitpid(philosopherPids[index]);
 	printf("Se retira %s\n", philosopherNames[index]);
